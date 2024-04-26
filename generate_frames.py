3bf8dacd88f7a835130a367a847a4f7812bd6ce8
DanPlayz0 <damuffin36@gmail.com>
2024-04-26 10:46:25 -0700

    Actually close after reading all frames

--- generate_frames.py
+++ generate_frames.py
@@ -8,13 +8,16 @@ print('Video loaded.')
 frameId = 0
 while True:
   ret, frame = cap.read()
-  if ret:
-    if frameId % 10 != 0: # Rendering only uses every 10th frame, so skip all others.
-      frameId += 1
-      continue
-    converted_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    with open(f"frames/{frameId}.json", "w") as f:
-      f.write(json.dumps(converted_frame.tolist()))
-      f.close()
-    print(f"Processed frame {frameId}")
+  if not ret:
+    break
+  if frameId % 10 != 0: # Rendering only uses every 10th frame, so skip all others.
     frameId += 1
+    continue
+  converted_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+  with open(f"frames/{frameId}.json", "w") as f:
+    f.write(json.dumps(converted_frame.tolist()))
+    f.close()
+  print(f"Processed frame {frameId}")
+  frameId += 1
+cap.release()
+print("Processed all frames! You may now run player.py")
